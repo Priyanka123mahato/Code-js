9491e899c5a6484a0248ce5c81f3b1947a27c8c1
Chandrani Mahato <97300996+Priyanka123mahato@users.noreply.github.com>
2023-08-25 04:36:25 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,100 @@
+/*
+let score1 =33
+let score2 ="33"
+let score3 ="33abc"
+
+console.log(typeof score1)//number
+console.log(typeof score2)//String
+console.log(typeof score3)//String
+
+console.log(typeof (score1))//number
+
+let valueInNumber=Number(score2)
+let valueInNumber1=Number(score3)
+console.log(typeof valueInNumber)//number type
+console.log(valueInNumber)//33-value
+console.log(typeof valueInNumber1)//number type
+console.log(valueInNumber1)//but value is not a number-->NaN
+
+
+let score =""
+let valueInNumber3=Number(score)
+console.log(typeof valueInNumber3)//number type
+console.log(valueInNumber3)//0--for empty string
+
+let score4=null
+console.log(typeof score4)//object type
+let valueInNumber2=Number(score4)
+console.log(typeof valueInNumber2)//number type
+console.log(valueInNumber2)//0--for null
+
+let score5=undefined
+console.log(typeof score5)//undefined type
+let valueInNumber5=Number(score5)
+console.log(typeof valueInNumber5)//number type
+console.log(valueInNumber5)//NaN-for undefined
+
+
+let score6=true
+console.log(typeof score6)//boolean type
+let valueInNumber6=Number(score6)
+console.log(typeof valueInNumber6)//number type
+console.log(valueInNumber6)//1
+
+//"33"=>33
+//""=>0
+//"33abc"=>NaN
+//true=>1; false=>0
+//null=>0
+//undefined=NaN
+*/
+
+/*
+//let isLoggedIn=1
+let isLoggedIn=0
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let isLoggedIn1=""
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+
+let isLoggedIn2="Swarnali"
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)
+*/
+
+/*
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+*/
+
+/*
+let value=3
+let negValue=-value
+//console.log(negValue);
+console.log(2+2)
+console.log(2-2)
+console.log(2*3)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)//8
+*/
+
+/*
+//not good approach
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(+true)//1
+console.log(+"")//0
+
+let gameCounter=100
+++gameCounter
+//gameCounter++
+console.log(gameCounter)
+*/
